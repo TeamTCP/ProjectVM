1f3696238bae0ae281cacbb147213910568552f3
soloplay <somnium1108@gmail.com>
2025-11-18 14:50:48 +0900

    Add: 보스 패턴 파일 추가
    - 메테오
    - 얼음
    - 불 소환(일직선 예상)

---
+++ Source/ProjectVM/AI/BTTask/BTTask_FrozenAttack.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_FrozenAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTVM_API UBTTask_FrozenAttack : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ProjectVM/AI/BTTask/BTTask_MeteorAttack.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_MeteorAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTVM_API UBTTask_MeteorAttack : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
